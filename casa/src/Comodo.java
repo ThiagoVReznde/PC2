f33a846eab2d8644ece6bdc43aa25f4d24900cac
Thiago Vinicius <67398127+ThiagoVReznde@users.noreply.github.com>
2025-06-26 14:24:42 -0300

    Update Comodo.java

--- casa/src/Comodo.java
+++ casa/src/Comodo.java
@@ -1,11 +1,19 @@
 
 
 public class Comodo{
+    private
     String nome;
     int area;
 
+    public
     Comodo(String nome, int area){
         this.nome = nome;
         this.area = area;
+    }
+    String get_nome(){
+        return nome;
+    }
+    int get_area(){
+        return area;
     }
 }
